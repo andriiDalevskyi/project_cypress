52890ffae407809cc7e6b935fcb466e347b778bb
andriiDalevskyi <andrew.dalevskyi@gmail.com>
2021-04-27 12:40:19 +0300

    update for not registered

--- cypress/integration/examples/gtnuxt/prodEnd2End/userNOTRegistered.js
+++ cypress/integration/examples/gtnuxt/prodEnd2End/userNOTRegistered.js
@@ -1,7 +1,7 @@
 describe('Not registered user end 2 end', function(){
     it('add delivery address before adding items', function(){
         cy.visit('https://www.getgoodtree.com/') // go to good tree
-        cy.get('.show > .modal-dialog > .modal-content > form > .modal-footer > .btn').click() //click to "I am 21+"
+        //cy.get('.show > .modal-dialog > .modal-content > form > .modal-footer > .btn').click() //click to "I am 21+"
         cy.get('.primary-nav > .nav-item > .nav-link').click()
         cy.url().should('include', '/products/flower')
         cy.wait(2000)
@@ -13,19 +13,39 @@ describe('Not registered user end 2 end', function(){
 
     it('redirect to sign up after adding adress and make purchase',function(){
         cy.get('#delivery').type('468 N Bedford Dr, Beverly Hills, CA 90210').click()
-        cy.wait(5000)
+        cy.wait(3000)
         cy.get('#delivery').click
         cy.get('body > div.pac-container.pac-logo.hdpi > div:nth-child(1)').click({force: true})
+        cy.wait(3000)
         cy.get('#jumbotron-content > .gt-btn').click()
-        cy.wait(5000)
+        cy.wait(3000)
         cy.get(':nth-child(1) > [style="position: absolute; bottom: 1px; left: 50%; z-index: 100;"] > .base-button > .svg-inline--fa').click()
         cy.scrollTo('top')
         cy.get('.tertiary-nav > .nav-item > .nav-link').click()
         cy.get('.sidebar-actions > .btn-submit').click()
         cy.url().should('include','/sign-up')
         })
+
+
     it('count items in the cart', function(){
         cy.contains('Cart 1 items')      
+        })
+
+    it('increase the number of product in the cart', function(){
+        cy.get('.tertiary-nav > .nav-item > .nav-link').click()
+        cy.get('.flex-row > :nth-child(3) > .svg-inline--fa > path').click()
+        cy.get('.flex-row > .text-center').should('have.text', ' 2 ')
     })
+    
+    it('continue shopping, add one more product', function(){
+        cy.get('.btn-close').click()
+        cy.get('.primary-nav > .nav-item > .nav-link').click()
+        cy.url().should('include', '/products/flower')
+        cy.wait(2000)
+        cy.get(':nth-child(2) > [style="position: absolute; bottom: 1px; left: 50%; z-index: 100;"] > .base-button > .svg-inline--fa > path').click()
+        cy.contains('Cart 2 items')
+    })
+
+
 
 })
--- cypress/integration/examples/gtnuxt/prodEnd2End/usetNOTRegisteredSorting.js
+++ cypress/integration/examples/gtnuxt/prodEnd2End/usetNOTRegisteredSorting.js
@@ -3,25 +3,15 @@
 describe('Not registered user end 2 end', function(){
     it('Sort by price',function(){
         cy.visit('https://www.getgoodtree.com/') // go to good tree
-        //cy.get('.show > .modal-dialog > .modal-content > form > .modal-footer > .btn').click() //click to "I am 21+"
+        cy.get('.show > .modal-dialog > .modal-content > form > .modal-footer > .btn').click() //click to "I am 21+"
         cy.get('.primary-nav > .nav-item > .nav-link').click() // go to products
         cy.wait(5000)
         cy.get('#__BVID__110').invoke('val', '43').trigger('change')
-        //cy.trainingXPath()
-        cy.xpath(`//*[@id="category-container"]/button[4]/div`)
-        .click()
-        
-        
-        
-            
-        //var arrayOfPrices = cy.get('//*[@id="category-home-router-view"]/div[2]/div[3]/div[2]/div/div[1]/div[2]/h4/text()')
-        //cy.get('[style="margin-left: 0.5em;"]').invoke('$43')
+        cy.get('.filters-container > :nth-child(2) > h5').should('have.text', ' Max price - $43')
 
 
-        //cy.get(`[id="#${menuNav[1]}$Menu"]`)
 
-
-// //*[@id="category-home-router-view"]/div[2]/div[3]/div[2]/div/div[1]/div[2]/h4/text()  <-locator for price list
+ //*[@id="category-home-router-view"]/div[2]/div[3]/div[2]/div/div[1]/div[2]/h4/text()  <-locator for price list
 
     })
 })
