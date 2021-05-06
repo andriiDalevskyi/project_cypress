eabded19f8ddbbc3c4e8da8710f8aa86629a09c9
andriiDalevskyi <44969315+andriiDalevskyi@users.noreply.github.com>
2021-05-06 18:01:57 +0300

    Update userNOTRegistered.js

--- cypress/integration/userNOTRegistered.js
+++ cypress/integration/userNOTRegistered.js
@@ -13,10 +13,10 @@ describe.only('Not registered user end 2 end', function(){
 
     it('redirect to sign up after adding adress and make purchase',function(){
         cy.get('#delivery')
-            .type("468 N Bedford Dr, Beverly Hills, CA 90210, USA", { delay: 100 })
+            .type("468 N Bedford Dr, Beverly Hills, CA 90210, USA", { delay: 10 })
         cy.get('[class=pac-item]').first().click()
         cy.get('#jumbotron-content > .gt-btn').click()
-        cy.wait(3000)
+        cy.wait(2000)
         cy.get(':nth-child(1) > [style="position: absolute; bottom: 1px; left: 50%; z-index: 100;"] > .base-button > .svg-inline--fa').click()
         cy.scrollTo('top')
         cy.get('.tertiary-nav > .nav-item > .nav-link').click()
@@ -39,7 +39,7 @@ describe.only('Not registered user end 2 end', function(){
         cy.get('.btn-close').click()
         cy.get('.primary-nav > .nav-item > .nav-link').click()
         cy.url().should('include', '/products/flower')
-        cy.wait(5000)
+        cy.wait(2000)
         cy.get(':nth-child(2) > [style="position: absolute; bottom: 1px; left: 50%; z-index: 100;"] > .base-button > .svg-inline--fa > path').click()
         cy.contains('Cart 2 items')
     })
